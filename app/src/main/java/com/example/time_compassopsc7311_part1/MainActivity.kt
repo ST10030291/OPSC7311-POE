35b183f03f146ad182560ddbb12666624dc75c1b
ST10091324 <st10091324@vcconnect.edu.za>
2024-05-02 19:37:29 +0200

    Added Firebase dependencies

--- app/src/main/java/com/example/time_compassopsc7311_part1/MainActivity.kt
+++ app/src/main/java/com/example/time_compassopsc7311_part1/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.time_compassopsc7311_part1
 
-import UserData
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
@@ -10,16 +9,21 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.time_compassopsc7311_part1.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
+    private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var sharedPreferences: SharedPreferences
     private lateinit var dateOfCreation: String
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
 
         // Initialize SharedPreferences
         sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
@@ -28,13 +32,6 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         binding.openSignUpActivity.setOnClickListener(this)
         binding.loginUser.setOnClickListener(this)
         binding.textViewForgotPassword.setOnClickListener(this)
-    }
-
-    // Function to search for a user by email and return their username
-    private fun getUsernameByEmail(email: String): String? {
-        val user = UserData.users.find { it.email == email }
-        dateOfCreation = user?.dateOfCreation.toString()
-        return user?.username ?: "Unknown user"
     }
 
     override fun onClick(v: View?) {
@@ -48,51 +45,49 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             // Logs user in if credentials are Valid
             R.id.loginUser -> {
                 // Get email and password
-                val editTextEmail = findViewById<EditText>(R.id.editTextEmailLogin)
-                val editTextPassword = findViewById<EditText>(R.id.editTextPasswordLogin)
+                val email = binding.editTextEmailLogin.text.toString()
+                val password = binding.editTextPasswordLogin.text.toString()
 
-                val enteredEmail = editTextEmail.text.toString()
-                val enteredPassword = editTextPassword.text.toString()
-
-                // Check if email / password are valid
-                val isCredentialsValid = UserData.users.any() { person ->
-                    person.email == enteredEmail && person.password == enteredPassword
-                }
-
-                if (isCredentialsValid) {
-                    Toast.makeText(this, "Login successful", Toast.LENGTH_SHORT).show()
-                    // Proceed to the Home Screen
-                    val username = getUsernameByEmail(enteredEmail)
-                    saveUserData(enteredEmail, username, dateOfCreation)
-                    val intent = Intent(this, HomeActivity::class.java)
-                    startActivity(intent)
-                    finish()
-                } else {
-                    Toast.makeText(this, "Incorrect email or password", Toast.LENGTH_SHORT).show()
-                }
-            }
-
-            R.id.textViewForgotPassword -> {
-                val editTextEmail = findViewById<EditText>(R.id.editTextEmailLogin)
-                val enteredEmail = editTextEmail.text.toString()
-
-                if (enteredEmail.isEmpty()) {
-                    Toast.makeText(this, "Please enter your email address", Toast.LENGTH_SHORT)
-                        .show()
-                } else {
-                    val user = UserData.users.find { it.email == enteredEmail }
-                    if (user != null) {
-                        // Display the users password
-                        Toast.makeText(
-                            this,
-                            "Your password is: ${user.password}",
-                            Toast.LENGTH_LONG
-                        ).show()
-                    } else {
-                        Toast.makeText(this, "User not found", Toast.LENGTH_SHORT).show()
+                if(email.isNotEmpty() && password.isNotEmpty()){
+                    firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this){task ->
+                        if(task.isSuccessful){
+                            Toast.makeText(this, "Login successful", Toast.LENGTH_SHORT).show()
+                            saveUserData(email, email, dateOfCreation)
+                            val intent = Intent(this, HomeActivity::class.java)
+                            startActivity(intent)
+                            finish()
+                        }
+                        else{
+                            Toast.makeText(this, "Incorrect email or password", Toast.LENGTH_SHORT).show()
+                        }
                     }
                 }
+                else{
+                    Toast.makeText(this, "Enter email and password", Toast.LENGTH_SHORT).show()
+                }
             }
+            // Update this for firebase
+//            R.id.textViewForgotPassword -> {
+//                val editTextEmail = findViewById<EditText>(R.id.editTextEmailLogin)
+//                val enteredEmail = editTextEmail.text.toString()
+//
+//                if (enteredEmail.isEmpty()) {
+//                    Toast.makeText(this, "Please enter your email address", Toast.LENGTH_SHORT)
+//                        .show()
+//                } else {
+//                    val user = UserData.users.find { it.email == enteredEmail }
+//                    if (user != null) {
+//                        // Display the users password
+//                        Toast.makeText(
+//                            this,
+//                            "Your password is: ${user.password}",
+//                            Toast.LENGTH_LONG
+//                        ).show()
+//                    } else {
+//                        Toast.makeText(this, "User not found", Toast.LENGTH_SHORT).show()
+//                    }
+//                }
+//            }
         }
     }
     private fun saveUserData(email: String, username: String?, dateofcreation: String) {
--- app/src/main/java/com/example/time_compassopsc7311_part1/SignUpActivity.kt
+++ app/src/main/java/com/example/time_compassopsc7311_part1/SignUpActivity.kt
@@ -1,29 +1,31 @@
 package com.example.time_compassopsc7311_part1
 
-import UserData
-import UserDetails
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
 import android.content.SharedPreferences
+import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
 import com.example.time_compassopsc7311_part1.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 
 class SignUpActivity : AppCompatActivity(), View.OnClickListener {
 
-    private val userList = mutableListOf<UserDetails>()
-
+    private lateinit var binding: ActivitySignUpBinding
     private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var firebaseAuth:FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivitySignUpBinding.inflate(layoutInflater)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
 
         // Initialize SharedPreferences
         sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
@@ -39,8 +41,7 @@ class SignUpActivity : AppCompatActivity(), View.OnClickListener {
                 val intent = Intent(this, MainActivity::class.java)
                 startActivity(intent)
             }
-            // Creates and saves a new user to a list
-            // This will be saved to the firebase database in the next part
+            // This will be saved to the firebase database
             R.id.buttonCreateAccount -> {
                 val editTextEmail = findViewById<EditText>(R.id.editTextEmailSignUp)
                 val editTextPassword = findViewById<EditText>(R.id.editTextPasswordSignUp)
@@ -53,33 +54,37 @@ class SignUpActivity : AppCompatActivity(), View.OnClickListener {
                 val enteredPassword = editTextPassword.text.toString()
                 val enteredUsername = editTextUsername.text.toString()
 
-                val user = UserData.users.find { it.email == enteredEmail }
-
                 if(enteredEmail.isEmpty() || enteredPassword.isEmpty() || enteredUsername.isEmpty()){
                     Toast.makeText(this, "Enter required details", Toast.LENGTH_SHORT).show()
                 }
-                else if(enteredPassword.length < 4) {
-                    Toast.makeText(this, "Password cannot be less than 3 characters", Toast.LENGTH_SHORT).show()
+                else if(enteredPassword.length < 6) {
+                    Toast.makeText(this, "Password cannot be less than 6 characters", Toast.LENGTH_SHORT).show()
                 }
                 else if (!enteredEmail.matches(emailPattern.toRegex())) {
                     Toast.makeText(this, "Enter a valid email address", Toast.LENGTH_SHORT).show()
                 }
-                else if (user != null){
-                    Toast.makeText(this, "User already exists", Toast.LENGTH_SHORT).show()
-                }
                 else {
+                    // Save a new user to FIREBASE
                     val currentDate = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()).format(
                         Date()
                     )
-                    val newUser = UserDetails(enteredEmail, enteredPassword, enteredUsername, currentDate)
-                    UserData.users.add(newUser)
-                    saveUserData(enteredEmail, enteredUsername, currentDate)
-                    Toast.makeText(this,"User Created Successfully", Toast.LENGTH_SHORT).show()
+                    Log.d("TAG", "Inside Saving User to FIREBASE")
 
-                    //Proceed to Home Screen
-                    val intent = Intent(this, HomeActivity::class.java)
-                    startActivity(intent)
-                    finish()
+                    firebaseAuth.createUserWithEmailAndPassword(enteredEmail,enteredPassword).addOnCompleteListener(this) {task ->
+                        if(task.isSuccessful){
+                            Toast.makeText(this,"User Created Successfully", Toast.LENGTH_SHORT).show()
+                            Log.d("TAG", "Inside Success msg")
+
+                            //Proceed to Home Screen
+                            val intent = Intent(this, HomeActivity::class.java)
+                            startActivity(intent)
+                            finish()
+                        }
+                        else{
+                            Log.d("TAG", "Inside unsuccessful msg")
+                            Toast.makeText(this,"SignUp Unsuccessful", Toast.LENGTH_SHORT).show()
+                        }
+                    }
                 }
             }
         }
